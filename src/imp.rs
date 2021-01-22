da97512b118b393634d58e02fbfb6e441765ae7c
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-01-22 15:44:25 +1100

    Highlight keywords, functions and types

--- src/imp.rs
+++ src/imp.rs
@@ -1,8 +1,28 @@
 use crate::palette::Palette;
+use mottle::theme::Scope::*;
 use mottle::theme::ThemeBuilder;
 
 pub(crate) fn add_rules(builder: &mut ThemeBuilder, palette: Palette) {
     builder.add_workspace_rule("foreground", palette.foreground());
     builder.add_workspace_rule("editor.foreground", palette.foreground());
     builder.add_workspace_rule("editor.background", palette.background());
+
+    builder.add_rule(Semantic("keyword"), palette.green());
+
+    builder.add_rules(&[Semantic("function"), Semantic("method")], palette.blue());
+
+    builder.add_rules(
+        &[
+            Semantic("type"),
+            Semantic("class"),
+            Semantic("struct"),
+            Semantic("enum"),
+            Semantic("enumMember"),
+            Semantic("union"),
+            Semantic("typeAlias"),
+            Semantic("builtinType"),
+            Semantic("typeParameter"),
+        ],
+        palette.yellow(),
+    );
 }
